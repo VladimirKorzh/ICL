e2ceeea5964b3659613d68b10103d3ebfb702f73
vladimir <vladimir@vmware-wonder.(none)>
2013-08-18 22:30:38 -0700

    ported exp calculation to mm

--- matchmaking/mm.py
+++ matchmaking/mm.py
@@ -1,7 +1,9 @@
 import Ice 
 Ice.loadSlice("/usr/share/slice/Murmur.ice", ['-I'+ Ice.getSliceDir()])
 import Murmur
-import sys, string, random, json
+import os, sys, string, random, json
+import urllib2
+import time, datetime, calendar
 
 from django.conf import settings
 from django.http import HttpResponse
@@ -133,11 +135,125 @@ class ICLMumble():
 	print user.name, 'is in Mumble, not using webauth. KICKED'
 	
 # END MUMBLE WRAPPER    
+
+
+# VAVLE API WRAPPER 
+class ValveApi():
+  def __init__(self):
+    self.cache = None
+    self.AllowNewQueries = True
     
+    self.CacheFile = 'valve_api_cache.json'
+    self.apikey = 'key=121A1CB1B99D9FA5C69DA2377E8B407F'    
+    self.apiurls = {}
+    self.apiurls['GetMatchHistory'] = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?'
+    self.apiurls['GetPlayerSummaries'] = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?' 
+    self.loadCache()    
     
+  def writeCache(self):
+      with open(self.CacheFile, 'w') as output:
+	  print 'Writing cache'
+	  json.dump(self.cache, output)
+
+  def loadCache(self):
+    if os.path.exists(os.path.join(os.getcwd(),self.CacheFile)):
+      with open(self.CacheFile, 'r') as input:
+	  print 'Loading cache'
+	  self.cache = json.load(input)
+    else:
+      self.cache = {}
+      
+  def query_api(self, api2query, conditions):
+    apiurl =  api2query       
+    finalurl = apiurl+self.apikey+conditions    
     
-   
+    if '&date_min=' in cachekey:
+      cachekey = cachekey[:-len('date_min=1374442863')]
+          
+    cachekey = datetime.date.today().isoformat() + apiurl + conditions
     
+    print "query:", finalurl
+    
+    # check if we have that query in cache
+    if cachekey in self.cache.keys():
+      #print 'Response found in cache',str(playerid),str(skill)
+      response = self.cache[cachekey]
+    else:  
+      response = None
+      if self.AllowNewQueries:
+	#print 'Response NOT found in cache',str(playerid),str(skill)
+	response = json.load(urllib2.urlopen(finalurl))
+	self.cache[cachekey] = response
+	time.sleep(1)	        
+    return response
+
+  def get_player_name_from_steamid(self, userid):
+      steamid = '&steamids=' + str(userid)      
+      response = self.query_api(self.apiurls['GetPlayerSummaries'], steamid)
+      personaname = None
+      if response != None:
+	personaname = response['response']['players'][0]['personaname']
+      return personaname
+
+
+  def get_player_exp_from_steamid(self,userid):
+    amount_of_games = {}
+    amount_of_games[1] = 0
+    amount_of_games[2] = 0
+    amount_of_games[3] = 0
+    
+    exp = 0
+
+    past = datetime.datetime.now() - datetime.timedelta(weeks = 4)
+    
+    timestamp = calendar.timegm(past.utctimetuple())
+    #print 'Calculating Exp starting from', past
+    
+    for skill in range(1,4):
+      
+      if skill == 1:
+	Q = 1
+      if skill == 2:
+	Q = 3
+      if skill == 3:
+        Q = 6
+      
+      conditions =  '&account_id=' + str(userid)
+      conditions += '&skill='+str(skill)
+      conditions += '&date_min='+str(timestamp) 
+      
+      response = self.query_api(self.apiurls['GetMatchHistory'], conditions)
+      if response is not None and response['result']['status'] == 1:
+	amount_of_games[skill] += response['result']['total_results']
+      else:
+	#print 'Error:', response['result']['statusDetail'][:25]	
+	break
+      exp += amount_of_games[skill] * Q
+    
+    total_games = amount_of_games[3]+amount_of_games[2]+amount_of_games[1]
+    if total_games != 0:
+      playerstats = {'name':self.get_player_name_from_steamid(userid), 'exp':exp,
+		      'n':amount_of_games[1], 'h':amount_of_games[2], 'vh':amount_of_games[3],
+		      'total': total_games}  
+      return playerstats
+            
+# Stats for 08/18/2013 
+# 646 taburetka^_^
+# 574 ICR.HoSi
+# 503 ICR.korzh
+# 461 ICR.Rock'n'Rolla
+# 429 ICR.kowka
+# 327 ICR.MARIO
+# 310 ICR.Immortal
+# 309 ICR.LuckyMan
+# 263 RockNaR
+# 236 ICR.Axe.Heart
+# 145 ICR.MiniSpy v.2.0
+
+
+
+
+
 # Steam auth interraction here 
 
 def getUserAvatarUrl(request):
