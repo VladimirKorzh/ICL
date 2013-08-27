446092ec90ae6a935dbadb9286419326c966f79a
vladimir <vladimir@wonder-server.(none)>
2013-08-27 09:48:59 -0700

    huge redesign of bot is underway

--- betsbot/jankbot.js
+++ betsbot/jankbot.js
@@ -1,65 +1,59 @@
 
-//Imports.
+// Import modules that we are using
 var fs         = require('fs');
 var Steam      = require('steam');
-var friends    = require('./bot_modules/friends.js');
-var logger     = require('./bot_modules/logger.js');
 var minimap    = require('minimap');
 var SteamTrade = require('steam-trade'); 
 var sqlite3    = require("sqlite3").verbose();
 
-// Define command line arguments.
-var argv = require('optimist');
+// Declare important variables that define the connection
+var USERNAME = 'korzhkorzhkorzh'
+var PASSWORD = '228121389'
+var DISPLAY_NAME = 'ICL.bot'
+var DICTIONARY_FILE = 'english.json'
 
-// Load config file.
-var CONFIG = JSON.parse(fs.readFileSync("config.json"));
-
-// Load dictionary.
-var DICT = JSON.parse(fs.readFileSync(CONFIG.dictionary));
-
-// Set admins.
-var ADMINS = CONFIG.admins;
-
-if (ADMINS == null) {
-  ADMINS = [];
-}
-
-// Load modules.
-var modules = [];
-for (var i = 0; i < CONFIG.modules.length; i++) {
-    modules.push(require("./bot_modules/" + CONFIG.modules[i] + ".js"));
-}
-
-// Global variables.
-var myName = CONFIG.displayName;
-
-var params = {}
-params['accountName'] = CONFIG.username
-params['password']    = CONFIG.password
-
-// load sentry hash file 
-require('fs').existsSync('sentry') ? params['shaSentryfile'] = require('fs').readFileSync('sentry') :  params['authCode'] = '5T529';
-
-// create instances of bot and trade apis and db connection
+// create instances of bot and trade APIs 
+// setup db connection as well
+var db = new sqlite3.Database("../icl/sqlite3.db");
 var bot = new Steam.SteamClient();
 var steamTrade = new SteamTrade();
-var db = new sqlite3.Database("../icl/sqlite3.db");
+
+// Login setup
+// load sentry hash file, which confirm that this is a valid
+// login to steam. Same is done by real steam client
+// in case if there is no sentry file, use the provided email password
+var params = {}
+require('fs').existsSync('sentry') ? params['shaSentryfile'] = require('fs').readFileSync('sentry') :  params['authCode'] = '5T529';
+params['accountName'] = USERNAME
+params['password']    = PASSWORD
 
 
+//////////////////////////////////////////////
 var bot_status = 'free'; // busy - when trading
 var current_task = '';   // holds current task of the bot
 var task_start_time = '';
 var actions = [];
 var maxnumtries = 3
+//////////////////////////////////////////////
 
 bot.on('debug', console.log);
 
-// Log in and set name
+// Log in
 bot.logOn(params['accountName'],params['password'], params['shaSentryfile'], params['authCode']);
 
-// happens after you log in.
+// Right after we logged on, we setup the bots name
+// and Online status
+bot.on('loggedOn', function() {
+	console.log('loggedOn');
+	bot.setPersonaState(Steam.EPersonaState.Online);
+	bot.setPersonaName(myName);
+});
+
+
+// Perform a web login and save session ID
+// which is used in trade APIs
 bot.on('webSessionID', function(sessionID) {
-	console.log('got a new session ID:', sessionID);
+	console.log('Got a new session ID:', sessionID);
 	steamTrade.sessionID = sessionID;
 	bot.webLogOn(function(cookies) {
 	  console.log('got a new cookie:', cookies);
@@ -69,148 +63,171 @@ bot.on('webSessionID', function(sessionID) {
 	});
 });
 
-bot.on('loggedOn', function() {
-	logger.log(DICT.SYSTEM.system_loggedin);
-	bot.setPersonaState(Steam.EPersonaState.Online);
-	bot.setPersonaName(myName);
-});
-
+// If the login was performed using an email password
+// save the sentry file for next time
 bot.on('sentry', function(sentry) {
 	console.log('Sentry received!');
 	require('fs').writeFileSync('sentry', sentry);
 });
 
+
+// we do not accept trades from people
+// we only send trade requests ourselves  
 bot.on('tradeProposed', function(tradeID, otherClient) {
-	// we are no longer accepting trades from people
-	// we only send trade requests ourselves  
 	console.log('tradeProposed, but we do not approve it.');
 });
 
+// Gets called when trade window appears on the screen
+// Here we place items that award to the player
 bot.on('sessionStart', function(otherClient) {
-	client = otherClient;
-	trade_window_items = []
-	
-	console.log('trading with ' + bot.users[client].playerName);
-	steamTrade.open(otherClient, function(){		  
-		  if (current_task.type == 'award') {
-			  steamTrade.loadInventory(570, 2, function(inv) {
-			      inventory = inv;
-			      itemsmatching = inv.filter(function(item) { 
-				      tags=item.tags;
-				      
-				      correct_item   = false;
-				      correct_rariry = false;
-				      
-				      tags.forEach(function(tag){  
-					      if (tag.category_name == 'Type') {
-						    if (tag.internal_name == 'DOTA_WearableType_Wearable') {
-							correct_item = true;
-						    }
-					      }
-					      if (tag.category_name == 'Rarity') {
-						    if ( tag.name == current_task.item_rarity ) {
-						      correct_rariry = true;
-						    }
-					      }
-					});
-				      
-				      if (correct_item == true && correct_rariry == true) {
-					      return true;
-				      }
-				      else {
-					      return false;
-				      }
-			      });      
-			      steamTrade.addItems(itemsmatching.slice(0, current_task.amount*2));
-			});
-		}
-		if (current_task.type == 'collect'){
-		      console.log('collecting')
-		}
-	});
+  console.log('sessionStart with', otherClient);
+
+  steamTrade.open(otherClient, function(){		  
+    if (current_task.type == 'award') {
+      console.log('Awarding player');
+      steamTrade.loadInventory(570, 2, function(inv) {      
+	  // find items that match our award
+	  itemsmatching = inv.filter(function(item) { 
+	      tags=item.tags;
+	      
+	      correct_item   = false;
+	      correct_rariry = false;
+	      
+	      tags.forEach(function(tag){  
+		  if (tag.category_name == 'Type') {
+			if (tag.internal_name == 'DOTA_WearableType_Wearable') {
+			    correct_item = true;
+			}
+		  }
+		  if (tag.category_name == 'Rarity') {
+			if ( tag.name == current_task.item_rarity ) {
+			  correct_rariry = true;
+			}
+		  }
+	      });
+	      
+	      // return true tells filter funciton that this item
+	      // matches our needs.
+	      if (correct_item == true && correct_rariry == true) {
+		      return true;
+	      }
+	      else {
+		      return false;
+	      }
+	    });      
+	    
+	    // make sure that we double the bet amout
+	    // FIX for the times when bot gave back only your bet
+	    steamTrade.addItems(itemsmatching.slice(0, current_task.amount*2));
+    });
+  }
+  if (current_task.type == 'collect'){
+    // if our task is to collect items than we simply wait for the user
+    // to place them in the trade window and react accordingly
+    console.log('Collecting');
+  }
+  });
 });
+
 
 steamTrade.on('offerChanged', function(added, item) {
-	task_start_time = Math.round(+new Date()/1000);
-	console.log('===> they ' + (added ? 'added ' : 'removed ') + item.name);
-	itemtags = item.tags;
-	
-	correct_item = false;
-	correct_rariry = false;
-	
-	itemtags.forEach(function(tag){      
-	      if (tag.category_name == 'Type') {
-		    if (tag.internal_name == 'DOTA_WearableType_Wearable') {
-			correct_item = true;
-		    }
-	      }    
-	      if (tag.category_name == 'Rarity') {
-		    if ( tag.name == current_task.item_rarity ){		      
-		      correct_rariry = true;
-		    }
-		    item_rarity_value = tag.name;
-	      }
-	});
-	  
-	if (!correct_item || !correct_rariry) {
-	      message = minimap.map({"itemrarity": item_rarity_value, "itemname": item.name},DICT.BET_RESPONSES.bet_incorrect_item_added);
-	      
-	      console.log(message);
-	      friends.messageUser(client, message, bot);	  
-	      return;
-	}
+    console.log('offerChanged ' + (added ? 'added ' : 'removed ') + item.name);
+    
+    // structure to hold items offered by trade partner
+    trade_window_items = []
+    
+    itemtags = item.tags;	
+    correct_item = false;
+    correct_rariry = false;
+    
+    // check the item's info to figure out if it is valuable and
+    // if it matches our needs
+    itemtags.forEach(function(tag){      
+	  if (tag.category_name == 'Type') {
+		if (tag.internal_name == 'DOTA_WearableType_Wearable') {
+		    correct_item = true;
+		}
+	  }    
+	  if (tag.category_name == 'Rarity') {
+		if ( tag.name == current_task.item_rarity ){		      
+		  correct_rariry = true;
+		}
+		item_rarity_value = tag.name;
+	  }
+    });
+      
+    // prepare the common part of the message
+    message = item_rarity_value + ' ' + item.name;
+    
+    if (!correct_item || !correct_rariry) {	      
+      msg = message + ' is not accepted. Wrong Rarity or Item Type.';
+      console.log(msg);
+      SteamTrade.chatMsg(msg);
+      return;
+    }
 
-	if (added) {
-	      trade_window_items.push(item);    
-	      message = minimap.map({"itemrarity": item_rarity_value, "itemname": item.name},DICT.BET_RESPONSES.bet_correct_item_added);
-	      
-	      console.log(message);
-	      friends.messageUser(client, message, bot);	  
-	}
-	if (!added) {
-	      var index = trade_window_items.indexOf(item);    
-	      trade_window_items.splice(index, 1);
-	      
-	      message = minimap.map({"itemrarity": item_rarity_value, "itemname": item.name},DICT.BET_RESPONSES.bet_correct_item_removed);	
-	      
-	      console.log(message);
-	      friends.messageUser(client, message, bot);
-	      
-	}
-	
-	message = minimap.map({"itemsleft": current_task.amount - trade_window_items.length, "itemrarity": current_task.item_rarity},DICT.BET_RESPONSES.bet_items_left);	
-	console.log(message);
-	friends.messageUser(client, message, bot);	
+    // if the user has added a valid item. 
+    if (added) {
+      trade_window_items.push(item);    	      
+      msg = message + ' is accepted.';
+      console.log(msg);
+      SteamTrade.chatMsg(msg);
+    }
+    
+    // if the user has removed a valid item
+    if (!added) {
+      // find that item in our structure and remove it
+      var index = trade_window_items.indexOf(item);    
+      trade_window_items.splice(index, 1);
+      
+      msg = message + ' was accepted. But you have removed it.';
+      console.log(msg);
+      SteamTrade.chatMsg(msg);
+    }
+    items_left = current_task.amount - trade_window_items.length
+    msg = 'Items left to add: ' + items_left;
+    console.log(msg);
+    SteamTrade.chatMsg(msg);
 });
 
 
-
+// This function gets called when the user presses the ready button
+// here we check that the deal is valid.
 steamTrade.on('ready', function() {
-      // amount of items doesn't match the bet 
+      // check if the amount of items does match the bet 
+      // in case if we are collecting items
       if (current_task.amount == trade_window_items.length && current_task.type =='collect') {
-	    steamTrade.ready(function() {
-	      console.log('confirming');
-	      steamTrade.confirm();
-	    });
+	  steamTrade.ready(function() {
+	    console.log('confirming');
+	    steamTrade.confirm();
+	  });
       }  
-      // if all goes through then we are good to go
+      
+      // if we are awarding a person, than we have already placed the right amount of items
+      // in the trade window in the sessionStart callback
       if (current_task.type == 'award') {
-	      steamTrade.ready(function() {
-		      message = minimap.map({"betid": current_task.bet_id}, DICT.BET_RESPONSES.bet_status_valid);
-		      console.log(message);
-		      friends.messageUser(client, message, bot);
-		      
-		      steamTrade.confirm();    
-	      });
+	  steamTrade.ready(function() {
+	    console.log('confirming');
+	    steamTrade.confirm();
+	  });
       }
 });
 
+// this function gets called after the trade is finished
+// result variable is supposed to hold the string representing
+// the status of the trade.
 steamTrade.on('end', function(result, items) {
+      console.log( result == 'complete' );
+      console.log( result === 'complete' );  
       console.log('trade', result.toString());
+            
+      // Since steam has a limitation on the amount of friend one might have
+      // we have to remove people from friends list after each transaction
       bot.removeFriend(current_task.uid);
-      if (result.toString() == 'complete') {
+      
+      if (result == 'complete') {    // TODO
 	    current_task = '';
-	    if (current_task.type == 'collect'){
+	    if (current_task.type == 'collect') {
 		  statement = "UPDATE matchmaking_bidder SET status='SUBMITTED' WHERE player_id ="+current_task.player_id+" AND bet_id="+current_task.bet_id;
 		  
 		  db.run(statement, function(err){
@@ -219,160 +236,38 @@ steamTrade.on('end', function(result, items) {
 		  
 		  console.log('marked as SUBMITTED');
 	    }
-      }
-      if (result == 'failed'){
-	console.log('failed');
+	    if (current_task.type == 'award') {  // TODO
+		statement = "UPDATE matchmaking_bidder SET status='AWARDED' WHERE player_id="+current_task.player_id+" AND bet_id="+current_task.bet_id;
+		console.log('marked as AWARDED');
+	    }
       }
 });
 
-// Respond to messages.
+// DON'T Respond to messages at all. Simply ignore them 
 bot.on('message', function(source, message, type, chatter) {
-
-      // Be sure this person is remembered and run friends list name update.
-      friends.addFriend(source);
-      friends.updateFriendsNames(bot);
-
-      // If the message is blank (blank messages are received from 'is typing').
-      if (message == '') return;
-
-      // Save the original full message for later use.
-      var original = message;
-      message = message.toLowerCase();
-      var fromUser = friends.nameOf(source);
-
-      // Log the received message.
-      logger.log(minimap.map({"user" : fromUser, "message" : original},DICT.SYSTEM.system_msg_received));
-
-      var input = message.split(" ");
-      
-      // check if this is an admin function request.
-      if (input[0] == "admin") {
-
-	// Authenticate as admin.
-	if (isAdmin(source)) {
-	      admin(input, source, original, function(resp) {
-		  friends.messageUser(source, resp, bot);
-	      });
-	      return;
-	}
-	else {
-	      friends.messageUser(source, DICT.ERRORS.err_not_admin, bot);
-	      return;
-	}
-      }
-      
-      // Placing a bet. 
-//       if (input[0] == DICT.CMDS.bet) {
-// 	    provided_betid = escape(input[1]);
-// 	    console.log('User asked to place a bet on: '+provided_betid);
-// 	    
-// 	    // check if there is a record about this bet in database
-// 	    statement = "SELECT id, item_rarity, amount, status FROM matchmaking_bet WHERE id="+provided_betid
-// 	    db.all(statement, function(err, rows) {
-// 	      
-// 		  // throw an error if encountered
-// 		  if (err) throw err;
-// 		      
-// 		  if (rows.length == 0) {
-// 		      // bet not found
-// 		      console.log(DICT.BET_RESPONSES.bet_not_found);
-// 		      friends.messageUser(source, DICT.BET_RESPONSES.bet_not_found, bot);
-// 		      return;
-// 		  }
-// 		  else if (rows.length == 1) {
-// 		      // bet is found
-// 		      console.log(DICT.BET_RESPONSES.bet_found);
-// 		      friends.messageUser(source, DICT.BET_RESPONSES.bet_found, bot);		  
-// 		      rows.forEach( function(row) {
-// 			  //check the status of this bet
-// 			  if (row.status != 'C') {
-// 				// if its still open or has other invalid status
-// 			      console.log(DICT.BET_RESPONSES.bet_not_closed); 
-// 			      friends.messageUser(source, DICT.BET_RESPONSES.bet_not_closed, bot);		  
-// 			      return;
-// 			  }
-// 			  else {
-// 			      // bet is found and has a closed status
-// 			      console.log(DICT.BET_RESPONSES.bet_status_valid); 
-// 			      friends.messageUser(source, DICT.BET_RESPONSES.bet_status_valid, bot);
-// 			      message = "Bet is " + row.amount + " " + row.item_rarity;
-// 			      friends.messageUser(source, message, bot);
-// 			      bet_id = provided_betid;   			   
-// 			      bet_itemrarity = row.item_rarity;
-// 			      bet_itemcount  = row.amount;
-// 			      return;
-// 			  }
-// 		      });
-// 		  }
-// 	    });
-// 	    return;
-// 	}  
-
-//   // Loop through other modules.
-//   for (var i = 0; i < modules.length; i++) {
-//     if (typeof modules[i].canHandle === 'function') {
-//       if (modules[i].canHandle(original)) {
-//         modules[i].handle(original, source, bot);
-//         return;
-//       }
-//     }
-//   }
-
-      // Default
-      friends.messageUser(source, randomResponse(), bot);
-
+    console.log('We have received a message.');
 });
-
 
 // Add friends automatically.
 bot.on('relationship', function(other, type){
       console.log('friend invite received!');
       if(type == Steam.EFriendRelationship.PendingInvitee) {
 	bot.addFriend(other);
-	logger.log(minimap.map({"userid" : other}, DICT.SYSTEM.system_added_friend));
-	friends.addFriend(other);
-	friends.updateFriendsNames(bot);
       }
 });
 
-
-// Responses for unknown commands.
-function randomResponse() {
-      var responses = DICT.random_responses;
-      return responses[Math.floor(Math.random() * responses.length)];
-}
-
-
-// Saves data and exits gracefully.
-function shutdown() {
-      // close connection to database
-      db.close();
-      friends.save();
-      
-      // exit from modules too
-      for (var i = 0; i < modules.length; i++) {
-	if (typeof modules[i].onExit === 'function') {
-	  modules[i].onExit();
-	}
-      }
-      // close process
-      process.exit();
-}
+// make sure we clean after ourselves
+process.on( 'SIGINT', function() {
+  console.log( "\nGracefully shutting down from  SIGINT (Crtl-C)" )
+  
+  // close connection to database
+  db.close();
+  process.exit( )
+})
 
 
-// Handler for admin functionality.
-function admin(input, source, original, callback) {
-    //   Quit function
-      if (input[1] == "quit") {
-	console.log(DICT.ADMIN.quit);
-	shutdown();
-      }
-}
 
-// Returns true if the given ID is an admin.
-function isAdmin(source) {
-      return ADMINS.indexOf(source) != -1;
-}
+
 
 
 function readdb() {
